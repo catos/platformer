cf23a1b1a11548067d5dbce1b4af99540a1d5ad2
Cato Skogholt <cskogholt@gmail.com>
2021-12-26 18:09:23 +0100

    pff

--- src/player.ts
+++ src/player.ts
@@ -1,20 +1,24 @@
 export default class Player {
   x: number
   y: number
+  width = 16
+  height = 24
   speed = 200
 
   constructor(x: number, y: number) {
     this.x = x
     this.y = y
+
+    
   }
 
   update(deltaTime: number) {
-    this.x += (this.speed * deltaTime)
-    this.y -= (this.speed * deltaTime)
+    // this.x += (this.speed * deltaTime)
+    // this.y -= (this.speed * deltaTime)
   }
   
   draw(context: CanvasRenderingContext2D) {
-    context.fillStyle = "#aaccee"
-    context.fillRect(this.x, this.y, 100, 100)
+    context.fillStyle = "#ff0000"
+    context.fillRect(this.x, this.y, this.width, this.height)
   }
 }
